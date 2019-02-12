82498bb97a5ab0a820ee80caababc9a50b647284
Ryan Sise <kerneltux0@gmail.com>
2019-02-12 17:04:21 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,18 @@
-def hello_t
-
+def hello_t(names)
+  if block_given?
+    i = 0
+    while i < names.size
+      yield(names[i])
+      i = i + 1
+    end
+    names
+  else
+    puts "Hey" No block was given!"
+  end
 end
 
-# call your method here!
-
+hello_t(names) do |name|
+  if name.start_with?("T")
+    puts "Hi, #{name}"
+  end
+end
