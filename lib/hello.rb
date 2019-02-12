1cc47ef7a1bb76a3294177bac3535dbffc5ad887
Ryan Sise <kerneltux0@gmail.com>
2019-02-12 17:06:37 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,8 +1,8 @@
-def hello_t(names)
+def hello_t(array)
   if block_given?
     i = 0
-    while i < names.size
-      yield(names[i])
+    while i < array.size
+      yield(array[i])
       i = i + 1
     end
     names
